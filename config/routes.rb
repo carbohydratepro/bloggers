d90c1115859514d4f891bc0a4db86c10f462e6f8
carbohydratepro <carbohydratepro@gmail.com>
2022-11-03 21:43:46 +0900

    ログアウトができない問題を修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'home/about', to: 'homes#about'
   devise_for :users
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root to: 'homes#top'
 
   resources :blogs, only: [:new, :index, :show, :create, :edit, :update, :destroy]
