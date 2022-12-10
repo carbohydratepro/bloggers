0a261b325e591dab2286d2ea2334a24274ac25d8
carbohydratepro <carbohydratepro@gmail.com>
2022-12-10 17:21:02 +0900

    ユーザー編集機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    # @user = User.find(params[:id])
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: 'User was successfully updated!'
+    else
+      render :edit
+    end
   end
 
   private
